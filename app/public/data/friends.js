089629689b7982092cbc2df43e38658565fb3444
Riseki <can1927@gmail.com>
2018-09-20 13:47:52 -0400

    added images

--- app/public/data/friends.js
+++ app/public/data/friends.js
@@ -2,22 +2,22 @@ var Data = [
   {
     Name: "Katie",
     Answers: [5, 2, 4, 1, 3, 2, 3, 1, 4, 5],
-    PhotoUrl: ""
+    PhotoUrl: "../images/katie.png"
   },
   {
     Name: "Spongebob",
     Answers: [5, 2, 3, 1, 1, 5, 5, 5, 5, 5],
-    PhotoUrl: ""
+    PhotoUrl: "../images/spongebob.jpeg"
   },
   {
     Name: "Ashley",
     Answers: [2, 3, 4, 4, 4, 4, 3, 1, 3, 5],
-    PhotoUrl: ""
+    PhotoUrl: "../images/ashley.jpg"
   },
   {
     Name: "Carl",
     Answers: [3, 2, 4, 4, 4, 2, 3, 1, 5, 5],
-    PhotoUrl: ""
+    PhotoUrl: "../images/carl.jpg"
   }
 ];
 
