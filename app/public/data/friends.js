0c0efcf5fe17c41b301a825f8eab7c2eb355ef94
Riseki <can1927@gmail.com>
2018-09-20 14:01:14 -0400

    Photo URL call

--- app/public/data/friends.js
+++ app/public/data/friends.js
@@ -2,22 +2,22 @@ var Data = [
   {
     Name: "Katie",
     Answers: [5, 2, 4, 1, 3, 2, 3, 1, 4, 5],
-    PhotoUrl: "../images/katie.png"
+    PhotoUrl: "./images/katie.png"
   },
   {
     Name: "Spongebob",
     Answers: [5, 2, 3, 1, 1, 5, 5, 5, 5, 5],
-    PhotoUrl: "../images/spongebob.jpeg"
+    PhotoUrl: "./images/spongebob.jpeg"
   },
   {
     Name: "Ashley",
     Answers: [2, 3, 4, 4, 4, 4, 3, 1, 3, 5],
-    PhotoUrl: "../images/ashley.jpg"
+    PhotoUrl: "./images/ashley.jpg"
   },
   {
     Name: "Carl",
     Answers: [3, 2, 4, 4, 4, 2, 3, 1, 5, 5],
-    PhotoUrl: "../images/carl.jpg"
+    PhotoUrl: "./images/carl.jpg"
   }
 ];
 
